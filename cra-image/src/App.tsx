89df36f11284641c8892937aa02b9f3be0c5d029
Erick Pascoal <erix_ps@hotmail.com>
2022-12-15 23:03:41 -0300

    feat: create component Image

---
+++ cra-image/src/App.tsx
@@ -0,0 +1,12 @@
+import { Image } from "./components/Image";
+
+function App() {
+  return (
+    <div>
+     <h1>teste</h1>
+     <Image src="https://sm.ign.com/ign_br/screenshot/default/goku_an6e.jpg" alt="goku" width={200} height={100}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ cra-image/src/components/Image/index.tsx
@@ -0,0 +1,17 @@
+// utils/image.js
+
+import React from "react";
+
+export const Image = ({ src, ...props }: any) => {
+  
+  try {
+  //@ts-ignore
+    return React.createElement(require.resolve("next/image").default, {
+      src: typeof src === "string" ? src : src.src,
+      ...props
+    });
+  } catch {
+    console.log("Not using Next.js");
+    return React.createElement("img", { src, ...props });
+  }
+};
